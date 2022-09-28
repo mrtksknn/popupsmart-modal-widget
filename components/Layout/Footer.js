3daa3bb1cdbfca5a36a879429ddea2bc999a29be
Mert keskin <mertkeskinmtk6@gmail.com>
2022-09-28 15:53:28 +0300

    created footer section for layout

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -1,8 +1,17 @@
+import Image from 'next/image';
+import Logo from '../../assets/others/logo.svg';
+
 const displayName = 'Footer';
 
 const Footer = () => (
-  <div className="footer">
-    Footer
+  <div className="footer flex flex-col my-20">
+    <Image
+      alt="logo" height="32" src={Logo}
+      width="32"
+    />
+    <span className="text-center mt-4 text-xs">
+      Powered by Popupsmart
+    </span>
   </div>
 );
 
