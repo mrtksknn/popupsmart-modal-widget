6f35b65b203707e02a9da5dce74155e6881eb7e0
Mert keskin <mertkeskinmtk6@gmail.com>
2022-10-05 20:30:23 +0300

    adding coming soon

--- components/Modal/index.js
+++ components/Modal/index.js
@@ -3,11 +3,18 @@ import Image from 'next/image';
 import tempModal from '../../assets/Modals/modal.svg';
 
 const Modal = () => (
-  <div className="mt-20">
-    <Image
-      alt="tempModal" height="400" src={tempModal}
-      width="740"
-    />
+  <div className="h-min flex flex-col items-center justify-center">
+    <div className="mt-20 soon">
+      <Image
+        alt="tempModal" height="400" src={tempModal}
+        width="740"
+      />
+    </div>
+    <div className="comingSoon-text">
+      <span className="text-4xl">
+        Coming Soon
+      </span>
+    </div>
   </div>
 );
 
